627f7afb1742c08e92839104c5b8bfea4bbcbc74
mac-mini <henry@henglin.com>
2026-04-30 16:43:54 +0800

    Update Homebrew formula for v0.1.0

--- packaging/homebrew-tap/Formula/gwtm.rb
+++ packaging/homebrew-tap/Formula/gwtm.rb
@@ -2,7 +2,7 @@ class Gwtm < Formula
   desc "Git worktree manager for local multi-project workflows"
   homepage "https://github.com/life2you/gwtm"
   url "https://github.com/life2you/gwtm/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "REPLACE_WITH_RELEASE_SHA256"
+  sha256 "5406fb3819c2dac954453e5f69fd3daddc8dc126b5c78c93c44fd53447adada4"
   license "MIT"
 
   depends_on "rust" => :build
